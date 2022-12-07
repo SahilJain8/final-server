1579f4bde0090d5c2c06aa3a2421a857df767e41
Kunal S <55314388+TheOfficialBug@users.noreply.github.com>
2022-12-07 18:59:44 +0530

    Changes made to elective subject codes of CSE Stream

--- Data.py
+++ Data.py
@@ -1,16 +1,16 @@
 cs_pro=[
-  {"name":"22ETC15A - Green Buildings","value":"Green Buildings"},
-  {"name":"22ETC15B - Introduction to Nano Technology","value":"Introduction to Nano Technology"},
-  {"name":"22ETC15C - Renewable Energy Sources","value":"Renewable Energy Sources"},
-  {"name":"22ETC15D - Introduction to Internet of Things (IOT)","value":"Introduction to Internet of Things (IOT)"},
-  {"name":"22ETC15E - Introduction to Cyber Security", "value":"Introduction to Cyber Security"}  
+  {"name":"22EMTC15A - Green Buildings","value":"Green Buildings"},
+  {"name":"22EMTC15B - Introduction to Nano Technology","value":"Introduction to Nano Technology"},
+  {"name":"22EMTC15C - Renewable Energy Sources","value":"Renewable Energy Sources"},
+  {"name":"22EMTC15D - Introduction to Internet of Things (IOT)","value":"Introduction to Internet of Things (IOT)"},
+  {"name":"22EMTC15E - Introduction to Cyber Security", "value":"Introduction to Cyber Security"}  
 ]
 
 cs_open=[
-  {"name":"22ESC141 - Introduction to Civil Engineering", "value":"Introduction to Civil Engineering"},
-  {"name":"22ESC142 - Introduction to Electrical Engineering", "value":"Introduction to Electrical Engineering"},
-  {"name":"22ESC143 - Introduction to Electronics Engineering", "value":"Introduction to Electronics Engineering"},
-  {"name":"22ESC144 - Introduction to Mechanical Engineering", "value":"Introduction to Mechanical Engineering"}
+  {"name":"22ENSC141 - Introduction to Civil Engineering", "value":"Introduction to Civil Engineering"},
+  {"name":"22ENSC142 - Introduction to Electrical Engineering", "value":"Introduction to Electrical Engineering"},
+  {"name":"22ENSC143 - Introduction to Electronics Engineering", "value":"Introduction to Electronics Engineering"},
+  {"name":"22ENSC144 - Introduction to Mechanical Engineering", "value":"Introduction to Mechanical Engineering"}
 ]
 
 
@@ -69,4 +69,3 @@ ece_pro=[
   {"name":"22PLC15C - Basics of JAVA Programming", "value":"Basics of JAVA Programming"},
   {"name":"22PLC15D - Introduction to C++ Programming", "value":"Introduction to C++ Programming"}
 ]
-
